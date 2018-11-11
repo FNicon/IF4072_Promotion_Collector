72cb6399f01f9832241c63d85dadf5ba08df9948
Luthfi Fadillah <luthfi.fadillah@outlook.com>
2018-11-11 23:20:01 +0700

    add info promo in web module

--- web/index.py
+++ web/index.py
@@ -1,6 +1,12 @@
 from flask import Flask,render_template
+import pandas as pd
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html')
+
+@app.route('/info_promo')
+def info_promo(info_promo=None):
+    info = pd.read_csv('../info_extract_data/info_extract.csv')
+    return render_template('info_promo.html', info_promo = info)
